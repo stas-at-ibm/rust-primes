7c09144c89b450385310dc6b3c3abfbbb12d93f5
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-10-18 11:58:30 +0200

    added todos, impl boundary getter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 // * Find primes starting from 100.000.000 with multi threads
-// * - [] check if number is prime
+// * - [x] check if number is prime
+// *    - [] refactor
 // * - [] define boundaries for each thread
 // * - [] create threads
 // *    - [] checks if a number is prime
 // *    - [] document result
 // *    - [] move to next number
 // *    - [] publish result
+// * - [] extract into modules
+// * - [] implement unit tests
 
 use colored::Colorize;
 
@@ -35,10 +38,46 @@ fn is_prime(n: u64) -> bool {
         return false;
     }
 
-    // let upper_boundary = (n as f32).sqrt() as u64;
+    let upper_boundary = (n as f32).sqrt() as u64;
 
-    // (19..=upper_boundary).step_by(2).all(|num| n % num != 0)
-    false
+    (19..=upper_boundary).step_by(2).all(|num| n % num != 0)
+}
+
+// highest_number: 100
+// threads_amount: 2
+// thread_num: 1
+// ----------
+// step = highest_number / threads_amount    // => 50
+// lower_bound = step * (thread_num - 1) + 1 // => 1
+// upper_bound = step * thread_num           // => 50
+// ----------
+// ----------
+// highest_number: 100
+// threads_amount: 2
+// thread_num: 2
+// ----------
+// step = highest_number / threads_amount    // => 50
+// lower_bound = step * (thread_num - 1) + 1 // => 51
+// upper_bound = step * thread_num           // => 100
+// ----------
+// ----------
+// highest_number: 101
+// threads_amount: 2
+// thread_num: 2
+// ----------
+// step = highest_number / threads_amount    // => 50,5 as u64 => 50
+// lower_bound = step * (thread_num - 1) + 1 // => 51
+// if threads_amount == thread_num { upper_bound = highest_number }
+// else upper_bound = step * thread_num      // => 100
+fn boundaries(thread_num: u64, threads_amount: u64, highest_number: u64) -> (u64, u64) {
+    let step: u64 = (highest_number / threads_amount) as u64;
+    let lower_bound: u64 = step * (thread_num - 1) + 1;
+
+    if threads_amount == thread_num {
+        (lower_bound, highest_number)
+    } else {
+        (lower_bound, step * thread_num)
+    }
 }
 
 fn main() {
