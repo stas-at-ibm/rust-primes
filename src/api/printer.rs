f859878f8e3823ba93673ecbabc10c760c31a370
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-10-28 14:46:05 +0200

    added alt print impl

--- src/api/printer.rs
+++ src/api/printer.rs
@@ -1,3 +1,4 @@
+use crate::model::positive_number::PositiveNumber;
 use colored::Colorize;
 
 pub fn print_prime_in_color(list_with_primes: Vec<(u64, bool)>) {
@@ -6,6 +7,16 @@ pub fn print_prime_in_color(list_with_primes: Vec<(u64, bool)>) {
             println!("{} is prime.", num.0.to_string().green());
         } else {
             println!("{} is {} prime.", num.0, "not".red());
+        }
+    }
+}
+
+pub fn print_positive_number_prime_in_color(list_with_primes: Vec<PositiveNumber>) {
+    for num in list_with_primes {
+        if num.is_prime() {
+            println!("{} is prime.", num.value().to_string().green());
+        } else {
+            println!("{} is {} prime.", num.value(), "not".red());
         }
     }
 }
