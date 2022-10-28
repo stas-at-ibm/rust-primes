0c9a21d516e89cb5a17e9ab7bb8fc8a00b850488
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-10-28 15:02:33 +0200

    added constructor

--- src/model/positive_number.rs
+++ src/model/positive_number.rs
@@ -4,6 +4,10 @@ pub struct PositiveNumber {
 }
 
 impl PositiveNumber {
+    pub fn new(value: u64, is_prime: bool) -> PositiveNumber {
+        PositiveNumber { value, is_prime }
+    }
+
     pub fn is_prime(&self) -> bool {
         self.is_prime
     }
