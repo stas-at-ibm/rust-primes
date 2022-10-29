0a4c6528dadbb54142c6b3e69daecf2a8c93dcc1
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-10-29 13:11:57 +0200

    added sender
    # added job struct

--- src/common/thread_pool.rs
+++ src/common/thread_pool.rs
@@ -1,12 +1,20 @@
-use std::thread::{self, JoinHandle};
+use std::{
+    sync::mpsc::{self, Sender},
+    thread::{self, JoinHandle},
+};
 
 pub struct ThreadPool {
     threads: Vec<Worker>,
+    sender: Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel::<Job>();
 
         let mut threads = Vec::with_capacity(size);
 
@@ -14,7 +22,7 @@ impl ThreadPool {
             threads.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { threads, sender }
     }
 
     pub fn execute<F>(&self, _f: F)
