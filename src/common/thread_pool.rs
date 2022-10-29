0af69ae5d51dc438c9cca8cc18d0e1ee45b57c03
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2022-10-29 13:04:21 +0200

    added worker

--- src/common/thread_pool.rs
+++ src/common/thread_pool.rs
@@ -1,13 +1,37 @@
-pub struct ThreadPool;
+use std::thread::{self, JoinHandle};
+
+pub struct ThreadPool {
+    threads: Vec<Worker>,
+}
 
 impl ThreadPool {
-    pub fn new(_size: usize) -> ThreadPool {
-        ThreadPool
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+
+        for id in 0..size {
+            threads.push(Worker::new(id));
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, _f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker { id, thread }
     }
 }
